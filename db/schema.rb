2bc3fc02a79533e6dbfd76600b805f6a18fbb88f
LoisKOUNINEF <lkouninef@gmail.com>
2022-03-23 16:27:04 +0100

    scaffold and migrate events

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_101738) do
+ActiveRecord::Schema.define(version: 2022_03_23_152538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.string "link_to"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "content"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
