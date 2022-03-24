ef78508d5bba613d1c2a5863d0b6fea6f9599ac4
enzostk <enzostk@gmail.com>
2022-03-24 10:00:06 +0100

    add admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :albums
+      resources :articles
+      resources :clubs
+      resources :events
+      resources :partners
+      resources :results
+      root to: "users#index"
+    end
+    
   devise_for :users
   root to: "articles#index"
   resources :results
