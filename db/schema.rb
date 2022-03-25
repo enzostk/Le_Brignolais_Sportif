2f8b1b1a22c56063db1f0869a3c17e6f2e9da22b
LoisKOUNINEF <lkouninef@gmail.com>
2022-03-25 08:28:27 +0100

    add user reference and lines for polymorphic in comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_061946) do
+ActiveRecord::Schema.define(version: 2022_03_25_072112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,10 @@ ActiveRecord::Schema.define(version: 2022_03_25_061946) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.integer "commented_on_id"
+    t.string "commented_on_type"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -90,4 +94,5 @@ ActiveRecord::Schema.define(version: 2022_03_25_061946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
