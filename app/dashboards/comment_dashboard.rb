30758db599fafd60bfd17e38cb05498649eb12c6
enzostk <enzostk@gmail.com>
2022-03-30 20:12:05 +0200

    update admin view, add created_at in articles/index, change class for name field for user's profile

--- app/dashboards/comment_dashboard.rb
+++ app/dashboards/comment_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class UserDashboard < Administrate::BaseDashboard
+class CommentDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -8,28 +8,14 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    user: Field::BelongsTo,
+    commented_on: Field::Polymorphic,
+    parent: Field::BelongsTo,
     comments: Field::HasMany,
     id: Field::Number,
-    email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
-    reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
-    confirmation_token: Field::String,
-    confirmed_at: Field::DateTime,
-    confirmation_sent_at: Field::DateTime,
-    unconfirmed_email: Field::String,
+    content: Field::Text,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    admin: Field::Boolean,
-    subscriber: Field::Boolean,
-    stripe_id: Field::String,
-    stripe_subscription_id: Field::String,
-    card_last4: Field::String,
-    card_exp_month: Field::Number,
-    card_exp_year: Field::Number,
-    card_type: Field::String,
-    name: Field::String,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -38,62 +24,34 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    user
+    commented_on
+    parent
     comments
-    id
-    email
-    encrypted_password
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    user
+    commented_on
+    parent
     comments
     id
-    email
-    encrypted_password
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    confirmation_token
-    confirmed_at
-    confirmation_sent_at
-    unconfirmed_email
+    content
     created_at
     updated_at
-    admin
-    subscriber
-    stripe_id
-    stripe_subscription_id
-    card_last4
-    card_exp_month
-    card_exp_year
-    card_type
-    name
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    user
+    commented_on
+    parent
     comments
-    email
-    encrypted_password
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    confirmation_token
-    confirmed_at
-    confirmation_sent_at
-    unconfirmed_email
-    admin
-    subscriber
-    stripe_id
-    stripe_subscription_id
-    card_last4
-    card_exp_month
-    card_exp_year
-    card_type
-    name
+    content
   ].freeze
 
   # COLLECTION_FILTERS
@@ -108,10 +66,10 @@ class UserDashboard < Administrate::BaseDashboard
   #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how users are displayed
+  # Overwrite this method to customize how comments are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(user)
-  #   "User ##{user.id}"
+  # def display_resource(comment)
+  #   "Comment ##{comment.id}"
   # end
 end
