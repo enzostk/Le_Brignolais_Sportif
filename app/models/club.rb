1f265ad29184c539a5ff4d256c1d3d08013a1ecd
enzostk <enzostk@gmail.com>
2022-04-01 12:19:19 +0200

    fixed partner selection for display

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,5 @@
 class Club < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
   validates :link_to, presence: true
   validates :image_url, url: true
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,6 +1,5 @@
 class Partner < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
   validates :link_to, presence: true
   validates :image_url, url: true
 end
