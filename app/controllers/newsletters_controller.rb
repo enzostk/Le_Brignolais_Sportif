ee2d0b04ad1d601e91157f3775681dc994525539
LoisKOUNINEF <lkouninef@gmail.com>
2022-04-01 03:10:39 +0200

    add newsletter migration model and controller

---
+++ app/controllers/newsletters_controller.rb
@@ -0,0 +1,24 @@
+class NewslettersController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_newsletter, only: [:destroy]
+
+  def create
+    @newsletter = Newsletter.create(newsletter_params)
+    if @newsletter.save
+      redirect_to article_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_newsletter
+    @newsletter = Newsletter.find(params[:id])
+  end
+
+  def newsletter_params
+    params.require(:newsletter).permit(:name, :email)
+  end
+
+end
